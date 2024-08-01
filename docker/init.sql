22e16f7a571956934a7f50b01a47d2f1300edf54
Timur Yambulatov <t.yambulatov@gmail.com>
2024-08-01 18:11:37 +0300

    chore: delete hello view and controller; move passwordEncoder bean to SecurityConfig; change database init data passwords to password hash; remove duplicate dependencies

--- docker/init.sql
+++ docker/init.sql
@@ -5,9 +5,10 @@ create table if not exists account (
 );
 
 insert into account (login, password)
-values ('login1', 'password1'),
-       ('login2', 'password2'),
-       ('login3', 'password3'),
-       ('login4', 'password4'),
-       ('login5', 'password5'),
-       ('login6', 'password6');
+values ('login', '{bcrypt}$2a$10$9i9QwTNdZASQ9fUkqAvE5u2epITMCroq14ltyYWPNhLdxzDJ/c7nG'),
+       ('login1', '{bcrypt}$2a$10$wZ.7Ez0KRehkJUWvos4sMOu9GUESSJKK7z2FF2QTp9AhAmjiuD1i6'),
+       ('login2', '{bcrypt}$2a$10$Zns9HkIvJSRHBRMlcyE9yeKJHIpUm9cZSEB91wZBDOr.asnG2nCJi'),
+       ('login3', '{bcrypt}$2a$10$BPX3ENFuZje99YHR.esJl.RN2KeO0b5oAMg2u8VM/9WWG9EM2mex.'),
+       ('login4', '{bcrypt}$2a$10$r2CEsoDSiD3MrW8gDVAGauh2NtD8vATMB9qoIn/ftLd0Bukhf7wMS'),
+       ('login5', '{bcrypt}$2a$10$Ooc.1FzRxSr3eN635PO8u.bfkySjZK63ZES20NKH9nRZF8j4dpB12'),
+       ('login6', '{bcrypt}$2a$10$gK9CsVX3l2NClsvxrzaMBuy9dP5BO99aIVfLxK3xUaMggcEabQM9W');
--- src/main/java/ru/timur/learning/configuration/security/config/SecurityConfig.java
+++ src/main/java/ru/timur/learning/configuration/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -44,5 +45,10 @@ public class SecurityConfig {
     public void bindUserDetailsService(AuthenticationManagerBuilder builder, PasswordEncoder passwordEncoder) throws Exception {
         builder.userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
