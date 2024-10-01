f9664f11557032db1eee6028561ebd4fe6a3572d
Timur Yambulatov <t.yambulatov@gmail.com>
2024-10-01 20:45:26 +0300

    chore: removed unnecessary annotations

--- src/main/java/ru/timur/learning/model/entity/ShotEntity.java
+++ src/main/java/ru/timur/learning/model/entity/ShotEntity.java
@@ -2,11 +2,9 @@ package ru.timur.learning.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.postgresql.geometric.PGpoint;
 
 @Data
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class ShotEntity {
     public enum Outcome {
